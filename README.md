d278828d02a8ddfd575af3fdad2fb7ad2f05f5b6
Meetkumar Naik <meetkumarnaik@gmail.com>
2026-01-24 11:12:29 +0000

    Set up project config and tools file and test folder

---
+++ README.md
@@ -0,0 +1,1 @@
+Kauri Pandit is a project to build an agent to play the traditional indian game known by the many names of Kaura-Baji/Matchi/Pachisi/Chowka-Bhara. 
---
+++ tests/test_example.py
@@ -0,0 +1,11 @@
+# tests/test_example.py
+
+def test_basic():
+    assert 1+1 == 2
+
+import pandas as pd
+import numpy as np
+
+print(pd.__version__)
+print(np.__version__)
+
